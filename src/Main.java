01752b481e4688c4f631925a13af70d18bf1a4fb
KoTaYava <146347467+KoTaYava@users.noreply.github.com>
2023-10-09 13:47:32 +0300

    home 3

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+       //Задача 1
+        int age = 21;
+        if (age > 18) {
+            System.out.println("Если возраст человека равен " + age + " то он совершеннолетний ");
+        } else{
+            System.out.println("Если возраст человека равен " + age + " то он несовершеннолетний ");
+        }
+     int t = 12;
+     if (t < 5 ){
+         System.out.println("На улице " +  t + " градусов,  нужно надеть шапку");
+     } else{
+         System.out.println("На улице " + t + " градусов, можно идти без шапки");
+     }
+
+    }
+}
